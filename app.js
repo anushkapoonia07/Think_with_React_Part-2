ba49a13a2c86a1db315ec37d91d95ff33bd09125
anushkapoonia07 <141112216+anushkapoonia07@users.noreply.github.com>
2023-11-18 11:43:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// REACT 2 | CHECK N CHECK
+
+// const container = document.getElementById('react-container');
+// ReactDOM.render("Hello! Welcome to React",container);
+
+// REACT 3 | IS IT A FUNCTION
+
+// const Container = () =>{
+//     return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+//        React.createElement(`div`,null,`Let's rock and roll`)
+//        );
+// }
+// const container = document.getElementById('react-container');
+// ReactDOM.render(React.createElement(Container),container);
+
+// REACT 4 | NOW IT'S CLASS TIME
+
+// class ReactContainer extends React.Component{
+//       render(){
+//           return React.createElement(`div`,null,`Hey Kalvians`,
+//           React.createElement(`div`,null,`Let's rock and roll with classes`)
+//           );
+//       }
+//   }
+//   const container = document.getElementById('react-container');
+//   ReactDOM.render(React.createElement(ReactContainer),container);
